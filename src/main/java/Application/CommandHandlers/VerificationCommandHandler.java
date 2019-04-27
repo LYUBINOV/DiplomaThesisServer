22bba45893c3c86fda1a95fd0ae44da79160ad2f
LYUBINOV <faustko@azet.sk>
2019-04-27 15:05:50 +0200

    [FEATURE] added integration with phpmyadmin db

--- src/main/java/Application/CommandHandlers/VerificationCommandHandler.java
+++ src/main/java/Application/CommandHandlers/VerificationCommandHandler.java
@@ -8,6 +8,8 @@ import org.bouncycastle.pqc.crypto.sphincs.SPHINCSPublicKeyParameters;
 import org.bouncycastle.util.encoders.Base64;
 import org.springframework.stereotype.Component;
 
+import java.sql.*;
+
 @Component
 public class VerificationCommandHandler {
     public String checkSign(VerificationDto verificationDto) {
@@ -27,6 +29,47 @@ public class VerificationCommandHandler {
             }
         }
 
+        insertToDB(verificationDto.getPublicKey(), verificationDto.getSignature(), verificationDto.getDocument());
+
         return "SignValid!";
+    }
+
+
+    public void insertToDB(String publicKey_PK, String signature, String document)
+    {
+        try
+        {
+            Class.forName("com.mysql.jdbc.Driver");
+
+//            https://remotemysql.com/phpmyadmin/sql.php?server=1&db=h9neXSyEba&table=SignedDocs&pos=0
+//            String url = "jdbc:mysql://remotemysql.com:3306/h9neXSyEba?autoReconnect=true&useSSL=false";
+            String url = "jdbc:mysql://remotemysql.com:3306/h9neXSyEba?useUnicode=true&characterEncoding=UTF-8&zeroDateTimeBehavior=convertToNull&serverTimezone=GMT&useSSL=false";
+            Connection c = DriverManager.getConnection(url, "h9neXSyEba", "knsiS0duEe");
+
+            PreparedStatement st = c.prepareStatement("insert into " + "SignedDocs" + " values (?,?,?,?)", Statement.RETURN_GENERATED_KEYS);
+
+            ResultSet rs = st.getGeneratedKeys();
+
+            Long id = null;
+            if(rs.next()) {
+                id = rs.getLong(1);
+            }
+            else { //inac je to prvz yaynam
+                id = 0L;
+            }
+
+            st.setLong(1, 0L);
+            st.setString(2, publicKey_PK);
+            st.setString(3, signature);
+            st.setString(4, document);
+
+            st.execute();
+            st.close();
+            c.close();
+        }
+        catch (ClassNotFoundException | SQLException e)
+        {
+            e.printStackTrace();
+        }
     }
 }
